872667a8be6783b9180950445b71bb272c797ebe
Aarti D'Cruz <aarti.dcruz@gmail.com>
2021-02-26 22:52:25 +0000

    progress

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,12 +1,69 @@
+let yoff = 0.0; // 2nd dimension of perlin noise
+
+function preload(){
+  sound = loadSound('music/pyramid_song.mp3');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  let cnv = createCanvas(400, 400);
+  cnv.mouseClicked(toggleSound);
+  amplitude = new p5.Amplitude();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(225, 224);
+  textAlign(CENTER);
+  text('tap to play', width/2, 20);
+
+  let level = amplitude.getLevel();
+  let size = map(level, 0, 1, 0, 200);
+  ellipse(size+10, yoff, size, size);
+  yoff += size*0.01;
+
+  for(let i = 0; i < width; i+=10){
+    line(0, i, size, size);
   }
-  ellipse(mouseX, mouseY, 80, 80);
+
+
+  // // We are going to draw a polygon out of the wave points
+  // beginShape();
+  //
+  // let xoff = 0; // Option #1: 2D Noise
+  // // let xoff = yoff; // Option #2: 1D Noise
+  //
+  // // Iterate over horizontal pixels
+  // for (let x = 0; x <= width; x += 10) {
+  //   // Calculate a y value according to noise, map to
+  //
+  //   // Option #1: 2D Noise
+  //   // let y = map(noise(xoff, yoff), 0, 1, 200, 300);
+  //   let y = map(xoff, size*yoff, 0, 1, 200, 300);
+  //
+  //   // Option #2: 1D Noise
+  //   // let y = map(noise(xoff), 0, 1, 200,300);
+  //
+  //   // Set the vertex
+  //   vertex(size, size);
+  //   // Increment x dimension for noise
+  //   xoff += 0.05;
+  // }
+  // // increment y dimension for noise
+  // yoff += 0.01;
+  // vertex(width, height);
+  // vertex(0, height);
+  // endShape(CLOSE);
+
 }
+
+function toggleSound(){
+  if (sound.isPlaying()) {
+    sound.stop();
+  } else {
+    sound.play();
+  }
+}
+
+
+
+
+// < --------------------------------- >
